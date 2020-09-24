de84c71c1332be2d05602bf62dfe9ef7c5b8b9cd
Joshua Fiallos <joshuafiallos1997@gmail.com>
2020-09-23 20:17:31 -0500

    Ejemplos de Javascript básicos

--- code/javascript/06_estructuras_respetitivas.js
+++ code/javascript/06_estructuras_respetitivas.js
@@ -1,3 +1,29 @@
-var estudiante = {
-    nombre = ''
+let persona = {
+    nombre: 'Alejandro',
+    apellido: 'Vega',
+    peso: 150,
 }
+
+const INCREMENTAR_PESO = 3
+const DECREMENTAR_PESO = 2
+
+const aumentarPeso = ( objeto ) => objeto.peso += INCREMENTAR_PESO
+const disminuyePeso = ( objeto ) => persona.peso -= DECREMENTAR_PESO
+
+const comeMucho = () => Math.random() < 0.1
+const realizaDeporte = () => Math.random() < 0.8
+
+const META = persona.peso - 10
+
+console.log( `Al inicio del año ${persona.nombre} pesa ${persona.peso}` )
+
+while( persona.peso > META ) {
+    if (comeMucho()) {
+        aumentarPeso()
+    }
+    if (realizaDeporte()) {
+        disminuyePeso()
+    }
+}
+
+console.log( `Al final del año ${persona.nombre} pesa ${persona.peso}` )
