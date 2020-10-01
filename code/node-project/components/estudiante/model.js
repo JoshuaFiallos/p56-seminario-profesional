fdbe9cf9fa5bf0ba8c025fa61b75f1168380659d
Joshua Fiallos <joshuafiallos1997@gmail.com>
2020-10-01 16:08:42 -0500

    Creación del componente docente

--- code/node-project/components/estudiante/model.js
+++ code/node-project/components/estudiante/model.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 const mySchema = new schema({
+    cedula: {
+        type: String,
+        required: true,
+    },
     nombre: {
         type: String,
         required: true,
     },
     apellido: {
-        type: String,
-        required: true,
-    },
-    correo_electronico: {
         type: String,
         required: true,
     },
